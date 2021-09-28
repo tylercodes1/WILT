20c6bb8db7cd4a669428932f6f8d0e40c25382e2
tkmoney11 <tylerkim@outlook.com>
2021-09-27 22:33:51 -0700

    Loader and Navbar Components

---
+++ components/Navbar.js
@@ -0,0 +1,41 @@
+import Link from 'next/Link'
+
+export default function Navbar() {
+  const user = true
+  const username = true
+
+  return (
+    <nav className='navbar'>
+      <ul>
+        <li>
+          <Link href='/'>
+            <button>FEED</button>
+          </Link>
+        </li>
+        {/* user is signed-in and has username */}
+        {username && (
+          <>
+            <li className='push-left'>
+              <Link href='/admin'>
+                <button className='btn-blue'>Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} alt='user photo' />
+              </Link>
+            </li>
+          </>
+        )}
+        {/* user  is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link href='/enter'>
+              <button className='btn-blue'>Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  )
+}
