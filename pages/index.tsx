b83a7e18e2e2cdd73362dff872fa589e181d9613
tkmoney11 <tylerkim@outlook.com>
2021-09-27 22:36:00 -0700

    Update base url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import Loader from './../components/Loader'
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Link
+        prefetch={false}
+        href={{
+          pathname: '/[username]',
+          query: { username: 'jeffd23' },
+        }}
+      >
+        <a>go to admin</a>
+      </Link>
+      <Loader show={true} />
+    </div>
+  )
 }
